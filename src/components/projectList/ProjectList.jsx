ccb6946bb1a460311a93834283b496049c497be2
Laz Austin <lazaustin94@gmail.com>
2025-09-27 18:36:54 -0500

    more color adjustments

--- src/components/projectList/ProjectList.jsx
+++ src/components/projectList/ProjectList.jsx
@@ -1,13 +1,18 @@
 import "./projectList.css";
 import Project from "../project/Project";
 import { projects } from "../../data";
+import { ThemeContext } from "../../context";
+import { useContext } from "react";
 
 const ProjectList = () => {
+
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
   return (
     <div className="pl" id="project">
       <div className="pl-wrapper">
         <div className="pl-texts">
-          <h1 className="pl-title">My Portfolio</h1>
+          <h1 className="pl-title" style={{color: darkMode ? "#FFC300" : "#001D3D"}}>My Portfolio</h1>
           <p className="pl-desc">
             Here are some hightlights of my work. Besides the site you're on of course.
           </p>
